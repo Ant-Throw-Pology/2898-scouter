db04d49a5b15288b0fa886d127dc450898040249
droid-kk11 <kavez121alt@icloud.com>
2024-12-21 14:35:22 -0800

    Add sign in page

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 import "./index.html" with {type: "asset"};
 import "./index.css" with {type: "asset"};
+
+document.getElementById("sid-continue")!.addEventListener("click", () => {
+    let role = (document.getElementById("scouter-role") as HTMLSelectElement).value;
+    if (role === "pit") {
+        alert("Pit scouter mode is currently unavailable.");
+    }
+
+    else {
+        alert("You have entered stand scouting mode! This should do something (soon).");
+    }
+});
