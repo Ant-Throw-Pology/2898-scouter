a6a02d209bfd8a941ba5dadadf07687ad0e9582b
droid-kk11 <153240992+droid-kk11@users.noreply.github.com>
2024-12-22 14:57:58 -0800

    Update version.py

--- version.py
+++ version.py
@@ -1,11 +1,13 @@
 stable = {
   "version_number": None,
   "version_name": None,
-  "version_changelog": None
+  "version_changelog": None,
+  "location": "seperate-folder"
 }
 
 prerelease = {
   "version_number": [1, 0, 0, "alpha1"],
   "version_name": "Initial Alpha",
-  "version_changelog": "Initial prerelease."
+  "version_changelog": "Initial prerelease.",
+  "location": "root"
 }
