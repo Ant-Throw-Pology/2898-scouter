1d9a8bcd1d97b41a48a75c8e87b48f09ed491f65
droid-kk11 <153240992+droid-kk11@users.noreply.github.com>
2024-12-22 14:52:40 -0800

    Add version reporting file
    The installer will read this file to determine the version when installing the app, and to see if any updates are available

---
+++ version.py
@@ -0,0 +1,11 @@
+stable = {
+  "version_number": None,
+  "version_name": None,
+  "version_changelog": None
+}
+
+prerelease = {
+  "version_number": [1, 0, 0, "alpha1"],
+  "version_name": "Initial Alpha",
+  "version_changelog": "Initial prerelease."
+}
