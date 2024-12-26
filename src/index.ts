4d107a32fc24694f34f650019d59290951dfe0ea
droid-kk11 <kavez121alt@icloud.com>
2024-12-26 12:01:30 -0800

    Start the prototype

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,35 @@ import { ce } from "./utils";
 
 document.getElementById("sid-continue")!.addEventListener("click", async () => {
     let role = (document.getElementById("scouter-role") as HTMLSelectElement).value;
+    let sid = (document.getElementById("scouter-id") as HTMLElement);
+    let tlist = (document.getElementById("teams-list") as HTMLElement);
     if (role === "pit") {
-        alert("Pit scouter mode is currently unavailable.");
+        //transition(sid, tlist);
+        alert("Pit scouting mode is currently unavailable");
     }
 
-    else {
-        await sleep(2);
-        alert("You have entered stand scouting mode! This should do something (soon).");
+    else if (role === "stand") {
+        transition(sid, tlist);
+    }
+
+    else if (role === "head") {
+        alert("Head scouting mode is currently unavailable.");
     }
 });
+
+async function transition(element1: HTMLElement, element2: HTMLElement) {
+    element1.style.animationName = "fade-out";
+    element1.style.animationDuration = "0.5s";
+    element1.style.animationTimingFunction = "linear";
+    await sleep(0.5);
+    element1.style.display = "none";
+    element2.style.animationName = "fade-in";
+    element2.style.animationDuration = "0.5s";
+    element2.style.animationTimingFunction = "linear";
+    element2.style.display = "block";
+    await sleep(0.5);
+    return;
+}
 
 function sleep(sec: number) {
     let ms = sec * 1000;
@@ -24,4 +44,8 @@ function greetUser(){
     let name = (document.getElementById("s-name") as HTMLInputElement).value;
     let first_name = name.split(" ")[0];
     greeting = `Welcome. ${first_name}`;
+}
+
+function notImplemented(){
+    alert("This function has not been implemented yet. Apologies.");
 }
